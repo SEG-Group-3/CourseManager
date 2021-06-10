b8b125f389edd3462a41d3dea6050d8b423f4d7f
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-06-10 14:22:49 -0400

    Updated DatabaseManager.java from Interface to Abstract class

--- app/src/main/java/com/segg3/coursemanager/DataBaseManager.java
+++ app/src/main/java/com/segg3/coursemanager/DataBaseManager.java
@@ -1,6 +1,31 @@
 package com.segg3.coursemanager;
 
-public interface DataBaseManager {
-    CollectionRefrence db;
-    DataBaseManager instance;
+import android.renderscript.RSInvalidStateException;
+import android.util.Log;
+
+import com.google.firebase.firestore.CollectionReference;
+
+public abstract class DataBaseManager
+{
+    private static CollectionReference db;
+    protected static DataBaseManager instance;
+
+    public static DataBaseManager getInstance()
+    {
+        throw new IllegalStateException();
+    }
+
+    protected DataBaseManager()
+    {
+        db.addSnapshotListener((snapshots, e) -> {
+            if (e != null) {
+                Log.w("TAG", "listen:error", e);
+                return;
+            }
+            readDataBase();
+        });
+        readDataBase();
+    }
+
+    abstract void readDataBase();
 }
