625ea573a59e193fbe5ba764a940cf5ea498fac4
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-06-11 20:41:18 -0400

    Added loginUser in AccountAccess.java

--- app/src/main/java/com/segg3/coursemanager/AccountAccess.java
+++ app/src/main/java/com/segg3/coursemanager/AccountAccess.java
@@ -1,9 +1,19 @@
 package com.segg3.coursemanager;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class AccountAccess extends DataBaseManager{
@@ -12,9 +22,11 @@ public class AccountAccess extends DataBaseManager{
 
     private final String PASSWORD_KEY = "password";
     private final String USERNAME_KEY = "userName";
+    private final String NAME_KEY = "name";
+    private final String EMAIL_KEY = "email";
     private final String TYPE_KEY = "type";
 
-    private List<User> users = new ArrayList<User>();
+    private User user;
 
 
     public static DataBaseManager getInstance()
@@ -27,7 +39,7 @@ public class AccountAccess extends DataBaseManager{
 
     private AccountAccess()
     {
-        super();
+        //super();
     }
 
     public void readDataBase()
@@ -36,9 +48,70 @@ public class AccountAccess extends DataBaseManager{
         throw new UnsupportedOperationException();
     }
 
+    private int counter = 0;
     public User loginUser(String password, String userName)
     {
-        throw new UnsupportedOperationException();
+        db.whereEqualTo(PASSWORD_KEY, password).whereEqualTo(USERNAME_KEY, userName).get()
+        .addOnCompleteListener
+        (
+            new OnCompleteListener<QuerySnapshot>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+
+                        Iterator<QueryDocumentSnapshot> i = task.getResult().iterator();
+
+                        if(i.hasNext())
+                        {
+                            QueryDocumentSnapshot document = i.next();
+                            if(document.contains(TYPE_KEY))
+                            {
+                                switch (document.get(TYPE_KEY, String.class))
+                                {
+                                    case "admin":
+                                        user = new Admin
+                                            (
+                                                document.getId(),
+                                                get(document, NAME_KEY),
+                                                get(document, EMAIL_KEY),
+                                                get(document, USERNAME_KEY),
+                                                null
+                                            );
+                                        break;
+                                    case "student":
+                                        user = new Student
+                                            (
+                                                document.getId(),
+                                                get(document, NAME_KEY),
+                                                get(document, EMAIL_KEY),
+                                                get(document, USERNAME_KEY),
+                                                null
+                                            );
+                                        break;
+                                    case "instructor":
+                                        user = new Instructor
+                                            (
+                                                document.getId(),
+                                                get(document, NAME_KEY),
+                                                get(document, EMAIL_KEY),
+                                                get(document, USERNAME_KEY),
+                                                null
+                                            );
+                                        break;
+                                }
+                            }
+
+                        }
+                    } else {
+                        Log.d("FIRE", "Error getting documents: ", task.getException());
+                    }
+                }
+            }
+        );
+
+        return null;
+
     }
 
     public boolean logOutUser()
