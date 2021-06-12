9657c59ed2ae71c45829c05ec81a3f3bc029695e
Fernando Nogueira <mrfernando2509@gmail.com>
2021-06-11 21:04:48 -0400

    Refactor Card View

---
+++ app/src/main/java/com/segg3/coursemanager/shared/CardViewHolder.java
@@ -0,0 +1,27 @@
+package com.segg3.coursemanager.shared;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.segg3.coursemanager.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CardViewHolder extends RecyclerView.ViewHolder{
+    public final ImageView imageView;
+    public final TextView title;
+    public final TextView subtitle;
+    public final TextView subsubtitle;
+
+    public CardViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+        title=itemView.findViewById(R.id.card_title);
+        subtitle=itemView.findViewById(R.id.card_subtitle);
+        subsubtitle=itemView.findViewById(R.id.card_subsubtitle);
+        imageView = itemView.findViewById(R.id.card_drawable_image);
+    }
+}
--- app/src/main/java/com/segg3/coursemanager/shared/courses/CourseListAdapter.java
+++ app/src/main/java/com/segg3/coursemanager/shared/courses/CourseListAdapter.java
@@ -3,19 +3,19 @@ package com.segg3.coursemanager.shared.courses;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.segg3.coursemanager.Course;
 import com.segg3.coursemanager.R;
+import com.segg3.coursemanager.shared.CardViewHolder;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.CourseViewHolder> {
+public class CourseListAdapter extends RecyclerView.Adapter<CardViewHolder> {
     private final List<Course> courseList;
     private View.OnClickListener onClickListener;
 
@@ -27,17 +27,17 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Co
     @NonNull
     @NotNull
     @Override
-    public CourseViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+    public CardViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.row_course_layout,parent,false);
         v.setOnClickListener(onClickListener);
-        return new CourseViewHolder(v);
+        return new CardViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull @NotNull CourseViewHolder holder, int position) {
-        holder.courseName.setText(courseList.get(position).name);
-        holder.courseCode.setText(courseList.get(position).code);
-
+    public void onBindViewHolder(@NonNull @NotNull CardViewHolder holder, int position) {
+        holder.title.setText(courseList.get(position).name);
+        holder.subtitle.setText(courseList.get(position).code);
+        holder.subsubtitle.setText(courseList.get(position).getId());
     }
 
     @Override
@@ -45,16 +45,5 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Co
         return courseList.size();
     }
 
-    public static class CourseViewHolder extends RecyclerView.ViewHolder{
-        private final TextView courseCode;
-        private final TextView courseName;
-        public CourseViewHolder(@NonNull @NotNull View itemView) {
-            super(itemView);
-            courseCode=itemView.findViewById(R.id.course_code_text);
-            courseName=itemView.findViewById(R.id.couse_name_text);
-        }
-
-
-    }
 
 }
