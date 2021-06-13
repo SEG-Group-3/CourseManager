38b2d16a9c3983117edea4ba5ece09a970332006
kareem <kqara103@uottawa.ca>
2021-06-13 05:06:45 +0300

    Add user image to home screen

--- app/src/main/java/com/segg3/coursemanager/shared/home/ui/HomeFragment.java
+++ app/src/main/java/com/segg3/coursemanager/shared/home/ui/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 
 import com.segg3.coursemanager.AccountAccess;
@@ -29,10 +30,20 @@ public class HomeFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        User u = ((AccountAccess)AccountAccess.getInstance()).getUser();
-        if (u != null ){
+        User u = ((AccountAccess) AccountAccess.getInstance()).getUser();
+        if (u != null) {
+            if (u.getType().equals("Student")) {
+                binding.imageView3.setImageDrawable(AppCompatResources.getDrawable(binding.imageView3.getContext(), R.drawable.ic_student));
+            } else if (u.getType().equals("Instructor")) {
+                binding.imageView3.setImageDrawable(AppCompatResources.getDrawable(binding.imageView3.getContext(), R.drawable.ic_instructor));
+            } else {
+                binding.imageView3.setImageDrawable(AppCompatResources.getDrawable(binding.imageView3.getContext(), R.drawable.ic_admin));
+            }
             binding.userNameField.setText(u.getUsername());
             binding.userTypeField.setText(u.getType());
         }
+
+
     }
 }
+
