fd4a4a5ae33d534bebdfd00184b1a4fd98f6703f
Fernando Nogueira <mrfernando2509@gmail.com>
2021-06-17 10:42:05 -0400

    Rewrite ViewModels

---
+++ app/src/main/java/com/segg3/coursemanager/shared/viewmodels/UsersViewModel.java
@@ -0,0 +1,32 @@
+package com.segg3.coursemanager.shared.viewmodels;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.segg3.coursemanager.shared.dao.UsersDao;
+import com.segg3.coursemanager.shared.models.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UsersViewModel extends ViewModel {
+    private LiveData<List<User>> users;
+    private final UsersDao dao = UsersDao.getInstance();
+
+    public LiveData<List<User>> getUsers() {
+        if (users == null) {
+            users = Transformations.switchMap(dao.getUsers(), this::loadUsers);
+        }
+        return users;
+    }
+
+    private LiveData<List<User>> loadUsers(HashMap<String, User> usersMap) {
+        MutableLiveData liveData = new MutableLiveData();
+        liveData.setValue(new ArrayList<>(usersMap.values()));
+        return liveData;
+    }
+}
