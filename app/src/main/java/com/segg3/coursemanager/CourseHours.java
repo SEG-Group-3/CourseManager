8ac1824d6e677d068641c621bf4087d79bef065b
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-06-24 18:08:55 -0400

    Updated Second Constructor in CourseHours.java

--- app/src/main/java/com/segg3/coursemanager/CourseHours.java
+++ app/src/main/java/com/segg3/coursemanager/CourseHours.java
@@ -3,6 +3,8 @@ package com.segg3.coursemanager;
 import java.time.DayOfWeek;
 import java.util.Date;
 
+import kotlin.collections.IntIterator;
+
 public class CourseHours {
 
     public final DayOfWeek weekDay;
@@ -18,8 +20,21 @@ public class CourseHours {
         this. durations = durations;
     }
 
-    public CourseHours (String CourseHoursRaw)
+    public CourseHours(String CourseHoursRaw)
     {
-        throw new UnsupportedOperationException();
+        String[] tmpParam = CourseHoursRaw.split("|");
+
+        this.weekDay = DayOfWeek.valueOf(tmpParam[0]);
+
+        String[] tmpDate = tmpParam[1].split(":");
+
+        Integer hour = new Integer(tmpDate[0]);
+        Integer min = new Integer(tmpDate[1]);
+
+        this.start = new Date(0,0,0, hour.intValue(), min.intValue());
+
+        Float duration = new Float(tmpParam[2]);
+
+        this.durations = duration.floatValue();
     }
 }
