284c4471d4f07ec971e27acefd0e9c0584e6d499
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-06-24 17:31:32 -0400

    added JavaDoc to Instructor

--- app/src/main/java/com/segg3/coursemanager/Instructor.java
+++ app/src/main/java/com/segg3/coursemanager/Instructor.java
@@ -6,21 +6,42 @@ public class Instructor extends User{
         super(userID, name, email, username, loginToken);
     }
 
+    /**
+     * Get an array courses based on a filter
+     * @param name String that is used to filter names
+     * @param feild firebase document feild that is used for filter
+     * @return
+     */
     public Course[] searchCourses(String name, String feild)
     {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Set capacity for a course
+     * @param course course that will be edited
+     * @param capacity new CourseHour to update date
+     */
     public void setCapacity(Course course, int capacity)
     {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Set Course hours of a course
+     * @param course course that will edited
+     * @param date new CourseHour to update date
+     */
     public void setCourseHours (Course course,  CourseHours date)
     {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Set description of a course
+     * @param course course that will edited
+     * @param desc String that will replace old desc
+     */
     public void setDesc(Course course, String desc)
     {
         throw new UnsupportedOperationException();
