dbcc10ad2884fedc26c3d756d34e9bde749042c8
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-06-24 17:06:04 -0400

    Created CourseHours class

---
+++ app/src/main/java/com/segg3/coursemanager/CourseHours.java
@@ -0,0 +1,14 @@
+package com.segg3.coursemanager;
+
+import java.time.DayOfWeek;
+import java.util.Date;
+
+public class CourseHours {
+
+    DayOfWeek weekDay;
+
+    Date start;
+
+    float durations;
+
+}
