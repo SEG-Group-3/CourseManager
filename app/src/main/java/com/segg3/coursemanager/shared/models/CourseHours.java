a23c707b170476fb0c0747928797d6fee3333700
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-06-25 18:36:04 -0400

    Added Time Class in CourseHours.java

--- app/src/main/java/com/segg3/coursemanager/shared/models/CourseHours.java
+++ app/src/main/java/com/segg3/coursemanager/shared/models/CourseHours.java
@@ -11,11 +11,63 @@ public class CourseHours {
 
     public final DayOfWeek weekDay;
 
-    public final Date start;
+    public final Time start;
 
     public final float durations;
 
-    public CourseHours(DayOfWeek weekDay, Date start, float durations)
+    public class Time
+    {
+        int hour;
+        int minute;
+
+        public Time(int hour, int minute)
+        {
+            setHour(hour);
+            setMinute(minute);
+        }
+
+        public Time(String timeRaw)
+        {
+            String[] tmp = timeRaw.split(":");
+
+            Integer hourRaw = new Integer(tmp[0]);
+            Integer minuteRaw = new Integer(tmp[1]);
+
+            setHour(hourRaw.intValue());
+            setMinute(minuteRaw.intValue());
+        }
+
+        private void setHour(int hour)
+        {
+            if(0 <= hour && hour < 24)
+            {
+                this.hour = hour;
+            }
+            else
+            {
+                throw new IllegalArgumentException("Illegal hour value. 0 <= hour < 24");
+            }
+        }
+
+        private void setMinute(int minute)
+        {
+            if(0 <= minute && minute < 60)
+            {
+                this.minute = minute;
+            }
+            else
+            {
+                throw new IllegalArgumentException("Illegal hour value. 0 <= hour < 24");
+            }
+        }
+
+        @Override
+        public String toString() {
+            return hour+":"+minute;
+        }
+    }
+
+    public CourseHours(DayOfWeek weekDay, Time start, float durations)
     {
         this.weekDay = weekDay;
         this.start = start;
@@ -28,12 +80,7 @@ public class CourseHours {
 
         this.weekDay = DayOfWeek.valueOf(tmpParam[0]);
 
-        String[] tmpDate = tmpParam[1].split(":");
-
-        Integer hour = new Integer(tmpDate[0]);
-        Integer min = new Integer(tmpDate[1]);
-
-        this.start = new Date(0,0,0, hour.intValue(), min.intValue());
+        this.start = new Time(tmpParam[1]);
 
         Float duration = new Float(tmpParam[2]);
 
@@ -43,6 +90,6 @@ public class CourseHours {
     @NotNull
     public String toString()
     {
-        return weekDay.name() + "|" + start.getHours() + ":" + start.getMinutes() + "|" + durations;
+        return weekDay.name() + "|" + start.toString() + "|" + durations;
     }
 }
