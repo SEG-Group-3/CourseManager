32e099e19b13d510f0096a67be382b111cede61f
Fernando Nogueira <mrfernando2509@gmail.com>
2021-07-01 10:30:13 -0400

    Add a "course hours" list to the instructor view

--- app/src/main/java/com/segg3/coursemanager/instructor/courses/ui/InstructorEditCourseFragment.java
+++ app/src/main/java/com/segg3/coursemanager/instructor/courses/ui/InstructorEditCourseFragment.java
@@ -1,7 +1,6 @@
 package com.segg3.coursemanager.instructor.courses.ui;
 
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,27 +13,40 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
 import com.segg3.coursemanager.R;
 import com.segg3.coursemanager.databinding.FragmentInstructorEditCourseBinding;
+import com.segg3.coursemanager.shared.adapters.CourseHoursListAdapter;
 import com.segg3.coursemanager.shared.dao.CoursesDao;
 import com.segg3.coursemanager.shared.models.Course;
+import com.segg3.coursemanager.shared.models.CourseHours;
 import com.segg3.coursemanager.shared.utils.UIUtils;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InstructorEditCourseFragment extends Fragment {
     FragmentInstructorEditCourseBinding binding;
     Course beingEdited;
+
+    CourseHoursListAdapter courseListAdapter;
+    RecyclerView recyclerView;
+    List<String> mutCourseHours;
+
     int position = -1;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentInstructorEditCourseBinding.inflate(inflater);
-        View view=binding.getRoot();
+        View view = binding.getRoot();
 
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), new OnBackPressedCallback(true) {
             @Override
@@ -59,12 +71,58 @@ public class InstructorEditCourseFragment extends Fragment {
 
         setHasOptionsMenu(true);
         UIUtils.setToolbarTitle(getActivity(), getString(R.string.edit_course));
-        String courseCode= getArguments().getString("code");
-        beingEdited=CoursesDao.getInstance().getCourse(courseCode);
+        String courseCode = getArguments().getString("code");
+        beingEdited = CoursesDao.getInstance().getCourse(courseCode);
         binding.courseCapacityInput.getEditText().setText(String.valueOf(beingEdited.capacity));
         binding.courseDescriptionInput.getEditText().setText(beingEdited.description);
 
         // TODO Populate course hours recycler view
+        recyclerView = binding.courseHoursRecyclerView;
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.scrollToPosition(0);
+
+        List<CourseHours> courseHours = new ArrayList<>();
+        for (String c : beingEdited.courseHours) {
+            courseHours.add(new CourseHours(c));
+        }
+
+        courseListAdapter = new CourseHoursListAdapter(courseHours, this::onCourseHourClicked);
+        recyclerView.setAdapter(courseListAdapter);
+
+
+        // Swipe stuff to the left to delete it
+        mutCourseHours = new ArrayList<>(beingEdited.courseHours);
+        InstructorEditCourseFragment parent = this;
+        ItemTouchHelper.SimpleCallback touchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+
+                return false;
+            }
+
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                mutCourseHours.remove(position);
+
+                List<CourseHours> courseHours = new ArrayList<>();
+                for (String c : mutCourseHours) {
+                    courseHours.add(new CourseHours(c));
+                }
+
+                courseListAdapter = new CourseHoursListAdapter(courseHours, parent::onCourseHourClicked);
+                recyclerView.setAdapter(courseListAdapter);
+
+                UIUtils.createToast(getContext(), "Swiped something...");
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(touchHelperCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+
         // binding.courseHoursRecyclerView...
 
         return view;
@@ -85,15 +143,28 @@ public class InstructorEditCourseFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
-    private void onCourseHourClicked(View v){
+    private void onCourseHourClicked(View v) {
         // TODO Update some course hour (maybe a popup menu?)
+
+        LayoutInflater inflater = getLayoutInflater();
+        View popupView = inflater.inflate(R.layout.dialog_course_hour, recyclerView, false);
+
+        new MaterialAlertDialogBuilder(getContext())
+                .setTitle("Pick a time")
+                .setView(popupView)
+                .setPositiveButton("Apply", (dialog, which) -> {
+                    // TODO Add a new button to the list of buttons to be added
+
+                } )
+                .setNegativeButton("Cancel", (dialog, which) -> {})
+                .show();
     }
 
-    private void onCancelEdit(View v){
+    private void onCancelEdit(View v) {
         UIUtils.swipeFragmentLeft(getParentFragmentManager(), new MyCourseViewFragment());
     }
 
-    private void onApplyEdit(View v){
+    private void onApplyEdit(View v) {
         // If invalid username or password, shake!
         boolean ok = true;
         TextInputLayout[] emptyCheckedFields = {binding.courseDescriptionInput, binding.courseCapacityInput};
@@ -109,17 +180,15 @@ public class InstructorEditCourseFragment extends Fragment {
 
         String capacity = binding.courseCapacityInput.getEditText().getText().toString();
         try {
-            if(Integer.parseUnsignedInt(capacity) <= 0){
+            if (Integer.parseUnsignedInt(capacity) <= 0) {
                 binding.courseCapacityInput.setError("Course capacity should be greater than one!");
                 binding.courseCapacityInput.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.shake));
             }
-        } catch (NumberFormatException exception){
+        } catch (NumberFormatException exception) {
             ok = false;
             binding.courseCapacityInput.setError("Not a number! exception");
             binding.courseCapacityInput.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.shake));
         }
-
-
 
 
         if (!ok)
@@ -129,6 +198,7 @@ public class InstructorEditCourseFragment extends Fragment {
         // TODO Update course date
         beingEdited.description = binding.courseDescriptionInput.getEditText().getText().toString();
         beingEdited.capacity = Integer.parseUnsignedInt(capacity);
+        beingEdited.courseHours = mutCourseHours;
         CoursesDao.getInstance().editCourse(beingEdited.code, beingEdited);
         UIUtils.swipeFragmentLeft(getParentFragmentManager(), new MyCourseViewFragment());
     }
