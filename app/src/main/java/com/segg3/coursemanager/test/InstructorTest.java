dab36c36def9a1a8fe4da2336c195953846d6099
Cameron Moreira <ctm313@gmail.com>
2021-07-01 13:40:07 -0400

    Update InstructorTest.java

--- app/src/main/java/com/segg3/coursemanager/test/InstructorTest.java
+++ app/src/main/java/com/segg3/coursemanager/test/InstructorTest.java
@@ -1,14 +1,29 @@
 package com.segg3.coursemanager.test;
 
+import com.google.firebase.FirebaseApp;
 import com.segg3.coursemanager.shared.models.Course;
 import com.segg3.coursemanager.shared.models.Instructor;
 
 import junit.framework.TestCase;
 
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class InstructorTest extends TestCase {
+//
+//    private FirebaseApp Firebase;
+//
+//    @Before
+//    public void beforeTest() {
+//        instrumentationContext = InstrumentationRegistry.getInstrumentation().context
+//        InstrumentationRegistry.getInstrumentation().targetContext;
+//
+//    }
+
+
     public void testCapacity() {
+
         Course newCourse = new Course();
         newCourse.name = "Software Engineering";
         newCourse.code = "SEG2105";
@@ -18,14 +33,14 @@ public class InstructorTest extends TestCase {
 
         assertEquals(expected.capacity, newCourse.capacity);
     }
-
-    public void testDescription() {
-        Course newCourse = new Course();
-        newCourse.description = "Welcome to Software Engineering!";
-        Instructor newInstructor = new Instructor();
-        Course expected = new Course();
-        String description = "Welcome to Software Engineering!";
-        newInstructor.setDesc(expected, description);
-        assertEquals(expected.description, newCourse.description);
-    }
+//
+//    public void testDescription() {
+//        Course newCourse = new Course();
+//        newCourse.description = "Welcome to Software Engineering!";
+//        Instructor newInstructor = new Instructor();
+//        Course expected = new Course();
+//        String description = "Welcome to Software Engineering!";
+//        newInstructor.setDesc(expected, description);
+//        assertEquals(expected.description, newCourse.description);
+//    }
 }
