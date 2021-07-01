ade50b2295fe047bed034844734a54ef5b396311
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-01 12:20:23 -0400

    Create CoursesDaoTest.java

---
+++ app/src/main/java/com/segg3/coursemanager/test/CoursesDaoTest.java
@@ -0,0 +1,10 @@
+package com.segg3.coursemanager.test;
+
+import junit.framework.TestCase;
+
+public class CoursesDaoTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+}
