76a99e60799284442384dd86f9407978edeea221
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-01 09:04:34 -0400

    Create CourseHoursTest.java

---
+++ app/src/main/java/com/segg3/coursemanager/test/CourseHoursTest.java
@@ -0,0 +1,11 @@
+package com.segg3.coursemanager.test;
+
+import junit.framework.TestCase;
+
+public class CourseHoursTest extends TestCase {
+
+    //@Test
+    public void testCompareTo() {
+        assertEquals(1,1);
+    }
+}
