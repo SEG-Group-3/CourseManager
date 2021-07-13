cd8f54f9becd2e74f5a3ec399f09b4b2a31ef517
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-13 18:43:48 -0400

    implement search method in NameFilter.java

--- app/src/main/java/com/segg3/coursemanager/shared/filter/NameFilter.java
+++ app/src/main/java/com/segg3/coursemanager/shared/filter/NameFilter.java
@@ -2,11 +2,31 @@ package com.segg3.coursemanager.shared.filter;
 
 import com.segg3.coursemanager.shared.models.Course;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NameFilter extends Filter<Course> {
     @Override
-    public List<Course> search(Object query) {
-        throw new UnsupportedOperationException();
+    public List<Course> search(Object query, List<Course> unfiltered) {
+        if(!(query instanceof String) || query == null)
+        {
+            throw new IllegalArgumentException();
+        }
+        String nameQuery = (String) query;
+
+        List<Course> tmp = new ArrayList<>();
+
+        Course c;
+
+        for(int i1 = 0; i1 < unfiltered.size(); i1++)
+        {
+            c = unfiltered.get(i1);
+            if(c.name.toLowerCase().contains(nameQuery.toLowerCase()))
+            {
+                tmp.add(c);
+            }
+        }
+
+        return tmp;
     }
 }
