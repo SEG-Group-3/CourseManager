178a29676dc05ab2210aabdc6c932d2b3990f488
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-13 18:53:29 -0400

    Implemented search in DayOfWeekFilter.java

--- app/src/main/java/com/segg3/coursemanager/shared/filter/DayOfWeekFilter.java
+++ app/src/main/java/com/segg3/coursemanager/shared/filter/DayOfWeekFilter.java
@@ -1,12 +1,48 @@
 package com.segg3.coursemanager.shared.filter;
 
 import com.segg3.coursemanager.shared.models.Course;
+import com.segg3.coursemanager.shared.models.CourseHours;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DayOfWeekFilter extends Filter<Course> {
     @Override
     public List<Course> search(Object query, List<Course> unfiltered) {
-        throw new UnsupportedOperationException();
+        if(query == null)
+        {
+            throw new NullPointerException();
+        }
+
+        if(!query.getClass().isArray())
+        {
+            throw new IllegalArgumentException();
+        }
+
+        boolean[] weekQuery = (boolean[]) query;
+
+        if(weekQuery.length != 7)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        List<Course> tmp = new ArrayList<Course>();
+        Course c;
+        CourseHours courseHours;
+        for(int i1 = 0; i1 < unfiltered.size(); i1++)
+        {
+            c = unfiltered.get(i1);
+            for(int i2 = 0; i2 < c.courseHours.size(); i2++)
+            {
+                courseHours = new CourseHours(c.courseHours.get(i2));
+
+                if(weekQuery[courseHours.weekDay.getValue() - 1])
+                {
+                    tmp.add(c);
+                }
+            }
+        }
+
+        return tmp;
     }
 }
